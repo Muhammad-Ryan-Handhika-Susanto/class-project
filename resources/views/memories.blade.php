4f4ad515dd65be294cde4fd665dfb8ed3ffbd6a7
Ridwan Annafi <kebanteruskntl@gmail.com>
2022-09-14 10:17:20 +0700

    galeri sementara

--- resources/views/memories.blade.php
+++ resources/views/memories.blade.php
@@ -1,7 +1,88 @@
 @extends('layouts.navbar')
 
 @section('content')
+<div class="section">
+    <div class="container">
+        <div class="title">
+            <h2>Memories of RPL</h2>
+            <p>description here</p>
+        </div>
+        <div class="gallery-container">
+        <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/800px-HTML5_logo_and_wordmark.svg.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+          
+          
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1017789323418030090/unknown.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+          
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://i.kym-cdn.com/entries/icons/facebook/000/039/484/cover7.jpg" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+          
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1010152209708748841/unknown.png?width=649&height=480" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
 
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1017789323418030090/unknown.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1017789323418030090/unknown.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1017789323418030090/unknown.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+                    <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="">
+                <img src="https://media.discordapp.net/attachments/744035173229789257/1017789323418030090/unknown.png" width="600" height="400">
+              </a>
+              <div class="desc">Add a description of the image here</div>
+            </div>
+          </div>
+        </div>
+          <div class="clearfix"></div>
+    </div>
+</div>
 @endsection
 
 <link rel="stylesheet" href="{{ 'assets/css/memories.css' }}">
