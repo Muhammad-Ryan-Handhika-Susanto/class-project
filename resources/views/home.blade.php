920468b7777aacdfe02e8330573a04a035812a5c
Muhammad-Ryan-Handhika-Susanto <ryanhandika712@gmail.com>
2022-09-18 12:31:24 +0700

    three upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,67 @@
-@extends('layouts.navbar')
+@extends('layouts.nav-teachers')
 
 @section('content')
+{{-- image slider start --}}
+<div class="slider">
+    <div class="slides">
+        <input type="radio" name="radio-btn" id="radio1">
+        <input type="radio" name="radio-btn" id="radio2">
+        <input type="radio" name="radio-btn" id="radio3">
+        <input type="radio" name="radio-btn" id="radio4">
+        <div class="slide first">
+            <img src="{{ 'assets/img/bg1.jpg' }}" alt="gambar 1">
+        </div>
+        <div class="slide">
+            <img src="{{ 'assets/img/bg2.jpg' }}" alt="gambar 2">
+        </div>
+        <div class="slide">
+            <img src="{{ 'assets/img/bg3.jpg' }}" alt="gambar 3">
+        </div>
+        <div class="slide">
+            <img src="{{ 'assets/img/bg3.jpg' }}" alt="gambar 4">
+        </div>
+        <div class="navigation-auto">
+            <div class="auto-btn1"></div>
+            <div class="auto-btn2"></div>
+            <div class="auto-btn3"></div>
+            <div class="auto-btn4"></div>
+        </div>
+    </div>
+    <div class="navigation-manual">
+        <label for="radio1" class="manual-btn"></label>
+        <label for="radio2" class="manual-btn"></label>
+        <label for="radio3" class="manual-btn"></label>
+        <label for="radio4" class="manual-btn"></label>
+    </div>
+</div>
 
+<div class="sambutan">
+    <h1>Selamat Datang Di Website RPL/PPLG</h1>
+</div>
+
+<div class="container">
+    <div class="pelajaran">
+        <h1>Bahasa pemrograman yang di gunakan</h1>
+    </div>
+    <div class="img">
+        <div class="col-sm-4">
+            <a href="#" class="thumbnail"><img src="{{ 'assets/img/html.png' }}" alt="HTML"></a>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    var counter = 1;
+    setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 4) {
+            counter = 1;
+        }
+    }, 4000);
+</script>
+{{-- image slider end --}}
 @endsection
 
 <link rel="stylesheet" href="{{ 'assets/css/home.css' }}">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
---
+++ resources/views/layouts/nav-teachers.blade.php
@@ -0,0 +1,25 @@
+{{-- Navbar Start --}}
+<nav>
+    <div class="navbar">PPLG_WEB</div>
+    <input type="checkbox" name="" id="click">
+    <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/teachers">Teachers</a></li>
+        <li><a href="/memories">Memories</a></li>
+        <li><a href="/achievements">Achievements</a></li>
+        <li><a href="/informations">Informations</a></li>
+    </ul>
+</nav>
+{{-- Navbar End --}}
+
+{{-- Content Start --}}
+@yield('content')
+{{-- Content End --}}
+
+{{-- Link Start --}}
+<link rel="stylesheet" href="{{ 'assets/css/navbar.css' }}">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+{{-- Link End --}}
